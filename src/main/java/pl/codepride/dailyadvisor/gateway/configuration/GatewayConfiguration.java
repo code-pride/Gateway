67b29fbe902880a31c49cffa87fecc328882a51f
benq95 <woj.bedkowski@gmail.com>
2018-10-09 18:50:01 +0200

    Added reactive spring cors filter.

--- src/main/java/pl/codepride/dailyadvisor/gateway/configuration/GatewayConfiguration.java
+++ src/main/java/pl/codepride/dailyadvisor/gateway/configuration/GatewayConfiguration.java
@@ -1,5 +1,6 @@
 package pl.codepride.dailyadvisor.gateway.configuration;
 
+import io.netty.handler.codec.http.cors.CorsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,9 @@ import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import pl.codepride.dailyadvisor.gateway.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class GatewayConfiguration {
@@ -18,13 +21,16 @@ public class GatewayConfiguration {
     private String frontendUrl;
 
     @Bean
-    public CorsFilter corsFilter() {
-        return CorsFilter.builder()
-                .allowedCredentials("true")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOrigin(frontendUrl)
-                .build();
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(frontendUrl);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge((long) 3600);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
